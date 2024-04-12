e8251993eb9b02df0fe0fb7563febf744487f410
Ian Bingham <ianbingham24@gmail.com>
2024-04-12 00:09:28 -0700

    refactor: delete unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-import "./App.css";
 import { SearchCompaniesShell } from "./features/company/SearchCompaniesShell";
 
 const queryClient = new QueryClient();
